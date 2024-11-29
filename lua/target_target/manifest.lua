0298f0d413a8bcd4dc60e413a279203af783632a
James Conigrave <james.conigrave@gmail.com>
2024-11-29 13:25:55 +1100

    make the manifest update a background process

--- lua/target_target/manifest.lua
+++ lua/target_target/manifest.lua
@@ -1,28 +1,40 @@
 -- lua/target_target/manifest.lua
 
+-- lua/target_target/manifest.lua
 local utils = require("target_target.utils")
 local Path = require("plenary.path")
+local Job = require("plenary.job")  -- Added plenary.job for asynchronous execution
 local data_dir = utils.get_data_dir()
 
 local M = {}
 
--- Function to update the manifest by calling R code
+-- Function to update the manifest by calling R code asynchronously
 function M.update_manifest()
     -- Command to run in R
-    local r_command = [[Rscript -e 'targets::tar_manifest() |> jsonlite::toJSON()']]
-
-    -- Run the R command and capture output
-    local manifest_json = vim.fn.system(r_command)
-
-    -- Handle errors
-    if vim.v.shell_error ~= 0 then
-        vim.notify("Error updating manifest: " .. manifest_json, vim.log.levels.ERROR)
-        return
-    end
-
-    -- Save the manifest to a file in the data directory
-    local manifest_path = Path:new(data_dir, "manifest.json")
-    manifest_path:write(manifest_json, "w")
+    local r_command = 'targets::tar_manifest() |> jsonlite::toJSON()'
+    -- Use plenary.job to run the R command asynchronously
+    Job:new({
+        command = 'Rscript',
+        args = { '-e', r_command },
+        on_exit = function(j, return_val)
+            if return_val == 0 then
+                local manifest_json = table.concat(j:result(), '\n')
+                -- Save the manifest to a file in the data directory
+                local manifest_path = Path:new(data_dir, "manifest.json")
+                manifest_path:write(manifest_json, "w")
+                -- Notify the user asynchronously
+                vim.schedule(function()
+                    vim.notify("Manifest updated successfully.", vim.log.levels.INFO)
+                end)
+            else
+                local stderr = table.concat(j:stderr_result(), '\n')
+                -- Notify the user of the error asynchronously
+                vim.schedule(function()
+                    vim.notify("Error updating manifest: " .. stderr, vim.log.levels.ERROR)
+                end)
+            end
+        end,
+    }):start()
 end
 
 -- Function to read the manifest
