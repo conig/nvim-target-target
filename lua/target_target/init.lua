056452071bdeb8601330391920b2d3f6e7110f3b
James Conigrave <james.conigrave@gmail.com>
2024-11-30 14:18:33 +1100

    fix pick target reference

--- lua/target_target/init.lua
+++ lua/target_target/init.lua
@@ -4,6 +4,6 @@ local M = {}
 
 M.acquire_target = require("target_target.acquire_target").acquire_target
 M.get_active_name = require("target_target.acquire_target").get_active_name
-M.pick_target = require("target_target.pick_target").pick_target
+M.pick_target = require("target_target.acquire_target").pick_target
 
 return M
